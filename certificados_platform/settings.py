f94b2810e5be5f4b9c0d42537507caf38ccd064b
Nelson Muquissi <emelsonmuquissi@gmail.com>
2025-03-07 13:20:44 +0100

    Atualizacões e sql ao invés de PostGresSql

--- certificados_platform/settings.py
+++ certificados_platform/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'certificados_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'certificados_db',
@@ -98,7 +98,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
-}
+}"""
 
 
 # Password validation
